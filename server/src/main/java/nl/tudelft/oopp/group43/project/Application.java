8ebe402cc19e9dd8b8cf96b50e9b8de595bec47f
Ziang Qiu <z.qiu@student.tudelft.nl>
2020-02-14 20:56:18 +0100

    finish the first api

--- server/src/main/java/nl/tudelft/oopp/group43/project/Application.java
+++ server/src/main/java/nl/tudelft/oopp/group43/project/Application.java
@@ -1,13 +1,13 @@
-package nl.tudelft.oopp.demo;
+package nl.tudelft.oopp.group43.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
