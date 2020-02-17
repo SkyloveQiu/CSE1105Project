79bd5b40ca19c689579b573228a7cd5fe3369bab
ThomBreugelmans <t.l.breugelmans@student.tudelft.nl>
2020-02-17 17:34:33 +0100

    Added JSON dependencies + making the client able to communicate with the personal server

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -2,17 +2,31 @@ package nl.tudelft.oopp.demo.controllers;
 
 import javafx.scene.control.Alert;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONObject;
 
 public class MainSceneController {
 
     /**
      * Handles clicking the button.
      */
+    @SuppressWarnings("unchecked")
     public void buttonClicked() {
+        JSONParser parser = new JSONParser();
+
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Quote for you");
+        alert.setTitle("An user for you");
         alert.setHeaderText(null);
-        alert.setContentText(ServerCommunication.getQuote());
+
+        try {
+            Object obj = parser.parse(ServerCommunication.getUser());
+            JSONObject user = (JSONObject) obj;
+            alert.setContentText("User: " + user.get("firstName") + "\nLastname: " + user.get("lastName"));
+        }
+        catch(ParseException e) {}
+
+
         alert.showAndWait();
     }
 }
