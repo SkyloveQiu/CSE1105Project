79b07fe7ff9100d6ccce0f1c2afc99c18be35b1a
Cristian Botocan <c.botocan@student.tudelft.nl>
2020-02-24 11:26:47 +0100

    LOGIN PAGE AND REGISTER PAGE
    The interface of login page + the interface of register page.
    The controller of login page + the controller for register page.
    The controller of register page contains method which are used to check if all field are filled.(it is a good start for sending JSON messgaes)
    
    - Issue with going to the next scene (from login page to register page) - it is not fixed yet.

---
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/LoginSceneController.java
@@ -0,0 +1,119 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SubScene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.views.RegisterDisplay;
+
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginSceneController  {
+
+  @FXML
+  private TextField email;
+  @FXML
+  private PasswordField password;
+
+
+
+   /* @FXML
+    public void initialize(URL url, ResourceBundle rb)
+    {
+
+    }*/
+
+
+    private String checkEmpty()
+  {
+      String emailString = email.getText();
+      String passwordString = password.getText();
+
+      if(emailString.isEmpty() || passwordString.isEmpty())
+      {
+          String message = new String();
+          if(emailString.isEmpty())
+              message = message + "You did not complete the email field" + "\n";
+          if(passwordString.isEmpty())
+              message = message + "You did not complete the password field";
+          return message;
+      }
+      else
+          return null;
+  }
+  @FXML
+  private void loginClicked(ActionEvent event)
+  {
+      String messageEmpty =  checkEmpty();
+      if(messageEmpty != null)
+      {
+          Alert alert = new Alert(Alert.AlertType.INFORMATION);
+          alert.setHeaderText(null);
+          alert.setContentText(messageEmpty);
+          alert.showAndWait();
+          return;
+      }
+
+      String emailString = email.getText();
+      String passwordString = password.getText();
+
+
+
+  }
+
+
+    //private Scene loginScene;
+
+   /* private void registerClicked()
+    {
+        System.out.println("mamaiNDN");
+    }*/
+
+
+    /*@SuppressWarnings("unchecked")
+    public void registerClicked()
+    {
+
+       // RegisterDisplay.main(new String [0]);
+      // System.out.println("am ajuns aici");
+        Parent nextRegisteView = FXMLLoader.load(getClass().getResource("/RegisterScene.fxml"));
+        Scene nextRegisterScene = new Scene (nextRegisteView);
+
+       Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(nextRegisterScene);
+        window.show();
+
+      System.out.println("amiwenfi");
+      
+   }*/
+    // @SuppressWarnings("unchecked")
+    @FXML
+    private void registerClicked(ActionEvent event) throws IOException
+    {
+
+       Parent nextRegisteView = FXMLLoader.load(getClass().getResource("RegisterScene.fxml"));
+
+        Scene nextRegisterScene = new Scene (nextRegisteView);
+
+       Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(nextRegisterScene);
+        window.show();
+    }
+
+
+}
