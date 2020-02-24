4dca6ce4a49d86a4047a4a95c75c70a7bc6c6cae
ThomBreugelmans <t.l.breugelmans@student.tudelft.nl>
2020-02-24 22:53:25 +0100

    Improvements to the Room Page
    + Methods for routing
    + Styling

--- client/src/main/java/nl/tudelft/oopp/group43/controllers/RoomPageSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/RoomPageSceneController.java
@@ -1,10 +1,13 @@
 package nl.tudelft.oopp.group43.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import nl.tudelft.oopp.group43.views.MainPageDisplay;
 
@@ -13,8 +16,17 @@ import java.net.URL;
 
 public class RoomPageSceneController {
 
+    private boolean clicked = false;
+
+    @FXML
+    private Button menubutton;
+
+    @FXML
+    private Pane menubar;
+
+    @FXML
     @SuppressWarnings("unchecked")
-    public void backToMainPage(ActionEvent e) throws IOException {
+    private void toMainPage(ActionEvent e) throws IOException {
 //        FXMLLoader loader = new FXMLLoader();
 //        URL xmlUrl = getClass().getResource("/mainPage.fxml");
 //        loader.setLocation(xmlUrl);
@@ -26,5 +38,36 @@ public class RoomPageSceneController {
 
         MainPageDisplay mp = new MainPageDisplay();
         mp.start((Stage) ((Node) e.getSource()).getScene().getWindow());
+    }
+
+    @FXML
+    @SuppressWarnings("unchecked")
+    private void toLoginPage(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/LoginScene.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Campus Management - Login Page");
+        stage.show();
+    }
+
+    @FXML
+    @SuppressWarnings("unchecked")
+    private void menuClicked(ActionEvent e) {
+        this.clicked = !this.clicked;
+
+//        Scene scene = (Scene) ((Node) event.getSource()).getScene();
+
+        if(clicked) {
+            menubar.relocate(0.0, 0.0);
+            menubutton.setText("Close");
+        }
+        else {
+            menubar.relocate(-180.0, 0.0);
+            menubutton.setText("Menu");
+        }
     }
 }
