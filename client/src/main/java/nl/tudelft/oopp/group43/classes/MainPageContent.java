92f305e5448e86615027cd3e22b420d78ccdefb5
ThomBreugelmans <t.l.breugelmans@student.tudelft.nl>
2020-02-25 21:37:03 +0100

    Threaded content loading of main page
    + Added classes folder for extra classes
    + Added content loader for main menu that is a Thread

---
+++ client/src/main/java/nl/tudelft/oopp/group43/classes/MainPageContent.java
@@ -0,0 +1,92 @@
+package nl.tudelft.oopp.group43.classes;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.group43.communication.ServerCommunication;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainPageContent implements Runnable {
+
+    private Stage stage;
+
+    public MainPageContent(Stage stage) {
+        this.stage = stage;
+    }
+
+    /**
+     * adds all buildings in the database to the main page
+     */
+    public void run() {
+        JSONParser json = new JSONParser();
+        GridPane gp = (GridPane) stage.getScene().lookup("#buildings_grid");
+
+        try {
+            JSONArray jsonArray = (JSONArray) json.parse(ServerCommunication.getBuilding());
+            EventHandler<MouseEvent> onClick = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    FXMLLoader loader = new FXMLLoader();
+                    URL xmlUrl = getClass().getResource("/roomPageScene.fxml");
+                    loader.setLocation(xmlUrl);
+                    Parent root = null;
+                    try {
+                        root = loader.load();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+                }
+            };
+
+            if(jsonArray.size() > 2) {
+                int size = jsonArray.size();
+                double rowHeight = (gp.getWidth() - 20.0) / 2;
+                while(size > 2) {
+                    RowConstraints rc = new RowConstraints();
+                    rc.setPrefHeight(rowHeight);
+                    rc.setMinHeight(rowHeight);
+                    rc.setMaxHeight(rowHeight);
+                    gp.getRowConstraints().add(rc);
+                    size -= 2;
+                }
+            }
+
+            int row = 0;
+            for(int i = 0; i < jsonArray.size(); i++) {
+                if(i % 2 == 0 && i != 0) { row++; }
+                JSONObject obj = (JSONObject) jsonArray.get(i);
+                Label label = new Label();
+                label.setPrefWidth(Integer.MAX_VALUE);
+                label.setMaxWidth(Integer.MAX_VALUE);
+                label.setMinWidth(0);
+                label.setStyle("-fx-background-color: #daebeb");
+                label.setText("Building " + obj.get("building_number") + ":\n" + obj.get("building_name"));
+
+                label.addEventFilter(MouseEvent.MOUSE_CLICKED, onClick);
+
+                gp.add(label, i % 2, row);
+            }
+
+            Label label = (Label) stage.getScene().lookup("#titlebar");
+            label.setText("Main Menu");
+        }
+        catch(ParseException e) {}
+    }
+}
