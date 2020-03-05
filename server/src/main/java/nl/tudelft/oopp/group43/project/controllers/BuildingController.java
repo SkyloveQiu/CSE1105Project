9f835757e3cf912ea78c17f109e56805f6c8b8a7
Ziang Qiu <z.qiu@student.tudelft.nl>
2020-03-05 14:03:39 +0100

    fix bugs

--- server/src/main/java/nl/tudelft/oopp/group43/project/controllers/BuildingController.java
+++ server/src/main/java/nl/tudelft/oopp/group43/project/controllers/BuildingController.java
@@ -1,23 +1,14 @@
 package nl.tudelft.oopp.group43.project.controllers;
 
-import com.sun.xml.bind.v2.TODO;
-import nl.tudelft.oopp.group43.project.models.Building;
-import nl.tudelft.oopp.group43.project.models.Room;
-import nl.tudelft.oopp.group43.project.repositories.BuildingRepository;
-import org.hibernate.HibernateException;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.TypedQuery;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.EntityManager;
+import nl.tudelft.oopp.group43.project.models.Building;
+import nl.tudelft.oopp.group43.project.repositories.BuildingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -39,14 +30,6 @@ public class BuildingController {
         repository.save(newBuilding);
         return "NEW BUILDING: " + newBuilding.getBuildingName();
     }
-
-
-
-    @DeleteMapping("building/{buildingId}")
-    @ResponseBody
-    public void removeFoodReservation(@PathVariable int buildingId) {
-        repository.deleteById(buildingId);
-    }
-
+    
 
 }
