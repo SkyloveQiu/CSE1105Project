2825d1fe9fecae8203481131761c18087ec7c652
Ziang Qiu <z.qiu@student.tudelft.nl>
2020-03-07 21:19:47 +0100

    no message

--- server/src/test/java/nl/tudelft/oopp/group43/project/models/UserTest.java
+++ server/src/test/java/nl/tudelft/oopp/group43/project/models/UserTest.java
@@ -2,12 +2,10 @@ package nl.tudelft.oopp.group43.project.models;
 
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserTest {
 
     @Test
     void getUsername() {
+         
     }
 }
