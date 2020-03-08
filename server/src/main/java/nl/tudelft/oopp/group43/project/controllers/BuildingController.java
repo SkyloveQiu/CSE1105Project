3e7bcd41b94261b80ed69043ba13ca2e0c1d3fde
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2020-03-08 22:07:13 +0100

    Add some security

--- server/src/main/java/nl/tudelft/oopp/group43/project/controllers/BuildingController.java
+++ server/src/main/java/nl/tudelft/oopp/group43/project/controllers/BuildingController.java
@@ -1,6 +1,7 @@
 package nl.tudelft.oopp.group43.project.controllers;
 
 import java.util.List;
+
 import nl.tudelft.oopp.group43.project.models.Building;
 import nl.tudelft.oopp.group43.project.repositories.BuildingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +30,33 @@ public class BuildingController {
     }
 
     // {"building_number":33,"building_name":"EWI","address":"32","opening_hours":"32"}
+
+    /**
+     *  Creates a new building.
+     * @param newBuilding describes a new building
+     * @return a message regarding the building
+     */
     @PostMapping(value = "/building")
     @ResponseBody
     public String createBuilding(@RequestBody Building newBuilding) {
+        if (repository.existsBuildingByBuildingNumber(newBuilding.getBuildingNumber())) {
+            return "BUILDING WITH NUMBER: " + newBuilding.getBuildingNumber() + " ALREADY EXISTS.";
+        }
+
         repository.save(newBuilding);
         return "NEW BUILDING: " + newBuilding.getBuildingName();
+    }
+
+    /**
+     *  Updates a new building.
+     * @param newBuilding describes an updated building
+     * @return a message regarding the building
+     */
+    @PostMapping(value = "/building/update")
+    @ResponseBody
+    public String updateBuilding(@RequestBody Building newBuilding) {
+        repository.save(newBuilding);
+        return "UPDATED BUILDING: " + newBuilding.getBuildingName();
     }
 
     /**
