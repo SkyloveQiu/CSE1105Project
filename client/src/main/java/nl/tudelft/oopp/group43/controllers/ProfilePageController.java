395d14df2d88c105ce7763a0bde8e2fe19d1b991
Niels van den Dool <n.vandendool@student.tudelft.nl>
2020-03-16 23:12:01 +0100

    Added a label, three buttons. Functionality not implemented yet.

---
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/ProfilePageController.java
@@ -0,0 +1,20 @@
+package nl.tudelft.oopp.group43.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ProfilePageController {
+
+    @FXML
+    private void showMyReservationsMenu(ActionEvent event) {
+    }
+
+    @FXML
+    private void changePassword(ActionEvent event){
+
+    }
+    @FXML
+    private void logOut(ActionEvent event) {
+
+    }
+}
