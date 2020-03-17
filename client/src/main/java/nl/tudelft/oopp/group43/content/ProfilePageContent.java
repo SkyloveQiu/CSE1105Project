48fb66312a08d343aca391aae549762fb8e2bd02
Niels van den Dool <n.vandendool@student.tudelft.nl>
2020-03-17 12:02:47 +0100

    When you click on the Change Password button, a pop-up pops up.

---
+++ client/src/main/java/nl/tudelft/oopp/group43/content/ProfilePageContent.java
@@ -0,0 +1,34 @@
+package nl.tudelft.oopp.group43.content;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.HPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class ProfilePageContent {
+
+    private static double windowHeight = 800;
+
+    public static void addContent(Scene scene) {
+        Button changePasswordButton = (Button) scene.lookup("#changePwd");
+        GridPane.setHalignment(changePasswordButton, HPos.RIGHT);
+
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                setWindowHeight((double) newValue);
+            }
+        });
+    }
+
+    public static void setWindowHeight(double height) {
+        windowHeight = height;
+    }
+
+    public static double getWindowHeight() {
+        return windowHeight;
+    }
+}
+
--- client/src/main/java/nl/tudelft/oopp/group43/controllers/ProfilePageController.java
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/ProfilePageController.java
@@ -1,20 +1,44 @@
 package nl.tudelft.oopp.group43.controllers;
 
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import nl.tudelft.oopp.group43.content.ProfilePageContent;
 
 public class ProfilePageController {
+
+    @FXML
+    private AnchorPane changePasswordMenu;
 
     @FXML
     private void showMyReservationsMenu(ActionEvent event) {
     }
 
     @FXML
-    private void changePassword(ActionEvent event){
+    private void showChangePasswordMenu(ActionEvent event){
+        TranslateTransition st = new TranslateTransition(Duration.seconds(1), changePasswordMenu);
+        st.setFromY(ProfilePageContent.getWindowHeight() + 40.0);
+        st.setToY(141.0);
+        st.setCycleCount(1);
 
+        st.play();
+        changePasswordMenu.setVisible(true);
     }
     @FXML
     private void logOut(ActionEvent event) {
 
+    }
+    @FXML
+    private void closeChangePasswordMenu(ActionEvent actionEvent) {
+        TranslateTransition trans = new TranslateTransition(Duration.millis(1000), changePasswordMenu);
+        trans.setFromY(141.0);
+        trans.setToY(ProfilePageContent.getWindowHeight() + 40);
+        trans.setCycleCount(1);
+
+        trans.setOnFinished(e -> changePasswordMenu.setVisible(false));
+        trans.play();
     }
 }
