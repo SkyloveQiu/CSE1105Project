d5d58aa813e1747a92002927b36ae48884947379
Niels van den Dool <n.vandendool@student.tudelft.nl>
2020-03-20 22:48:03 +0100

    Added checkbox with buildings in it. (Maybe we can make a general place to import buildings from, instead of code duplication.)

--- client/src/main/java/nl/tudelft/oopp/group43/content/FoodPageContent.java
+++ client/src/main/java/nl/tudelft/oopp/group43/content/FoodPageContent.java
@@ -1,11 +1,40 @@
 package nl.tudelft.oopp.group43.content;
 
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import nl.tudelft.oopp.group43.communication.ServerCommunication;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
 
 public class FoodPageContent {
 
-    public static void addContent(Scene scene) {
-        return;
+    private static Scene scene;
+
+    public static void addContent(Scene currentScene) {
+        scene = currentScene;
+        addBuildings();
     }
 
+    private static void addBuildings() {
+        ChoiceBox<String> returnBuildingList = (ChoiceBox<String>) scene.lookup("#returnBuildingList");
+        JSONParser json = new JSONParser();
+        try {
+            JSONArray array = (JSONArray) json.parse(ServerCommunication.getBuilding());
+            ArrayList<String> buildings = new ArrayList<String>();
+            for (Object obj : array) {
+                JSONObject jsonObject = (JSONObject) obj;
+                buildings.add((String) jsonObject.get("building_name"));
+            }
+
+            returnBuildingList.setItems(FXCollections.observableArrayList(buildings));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
