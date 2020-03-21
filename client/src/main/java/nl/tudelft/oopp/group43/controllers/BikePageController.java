055770431588911f3ebfddf48d5613af4a3ce6b2
Cristian Botocan <c.botocan@student.tudelft.nl>
2020-03-21 22:27:55 +0100

    32 - RESERVE BIKE PAGE - FUNCTIONALITY OVERLAY
    - A user is able to select and reserve a bike. He will receive a list with bikes from a specific building. When he will press the button "Reserve Bike" he will receive a message with the id of the bike which is available.
    - The communication between the server and the client regarding this data is also done.
    - There is a check if the user is login in -> he will be redirected to the login page
    - There is a check if the user selected a building -> he will receive an alert regarding it
    - There is a check if the user selected a building with available bikes -> he will receive an alert regarding it
    - The checksyle is also done and java doc for each method.
    
    Resolves: #issue 32 - done

--- client/src/main/java/nl/tudelft/oopp/group43/controllers/BikePageController.java
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/BikePageController.java
@@ -1,11 +1,25 @@
 package nl.tudelft.oopp.group43.controllers;
 
+import java.io.IOException;
+
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+import nl.tudelft.oopp.group43.communication.ServerCommunication;
 import nl.tudelft.oopp.group43.content.BikePageContent;
+import nl.tudelft.oopp.group43.sceneloader.SceneLoader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
 
 public class BikePageController {
 
@@ -15,6 +29,7 @@ public class BikePageController {
     private AnchorPane returnMenu;
     @FXML
     private AnchorPane reserveMenu;
+
 
     @FXML
     private void showReturnBikeMenu(ActionEvent event) {
@@ -80,6 +95,59 @@ public class BikePageController {
 
         trans.setOnFinished(e -> reserveMenu.setVisible(false));
         trans.play();
+    }
+
+    /**
+     * /**
+     * Checks if a building is selected and give to the user an available bike for that building (if it is possible).
+     *
+     * @param event - the button "Rent" is pressed.
+     * @throws ParseException - if something goes wrong with the JSON parser.
+     * @throws IOException    - if something oes wrong moving to the Login Page.
+     */
+    @FXML
+    private void confirmRent(ActionEvent event) throws ParseException, IOException {
+        String selectedBuilding = BikePageContent.getSelectedBuilding();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        String alertInformation;
+        if (ServerCommunication.getToken().equals("invalid")) {
+            alertInformation = "You are not log in yet! Please login!";
+            alert.setContentText(alertInformation);
+            alert.showAndWait();
+            SceneLoader.setScene("login");
+            SceneLoader sl = new SceneLoader();
+            sl.start((Stage) ((Node) event.getSource()).getScene().getWindow());
+            return;
+        }
+        boolean ok = false;
+        if (selectedBuilding == null) {
+            alertInformation = "You have not selected any building!" + "\n" + " Please choose one!";
+        } else {
+            System.out.println(selectedBuilding);
+            String bikes = ServerCommunication.getBikeRenting(selectedBuilding);
+            JSONParser parser = new JSONParser();
+            JSONArray jsonArray = (JSONArray) parser.parse(bikes);
+            if (jsonArray.size() == 0) {
+                alertInformation = "This building doesn't have any available bikes!";
+            } else {
+                JSONObject obj = (JSONObject) jsonArray.get(0);
+
+                String bikeID = Long.toString((Long) obj.get("bikeId"));
+                String message = ServerCommunication.sendBikeRenting(bikeID);
+                if (!message.equals("OK")) {
+                    alertInformation = "Check your connection with the server! Something goes wrong with it";
+                } else {
+                    alertInformation = "Your reservation was successful proceed!" + "\n" + "You reserved the bike with ID  " + obj.get("bikeId");
+                    ok = true;
+                }
+            }
+
+        }
+        alert.setContentText(alertInformation);
+        alert.showAndWait();
+        if (ok) {
+            closeRentMenu(event);
+        }
     }
 
 }
