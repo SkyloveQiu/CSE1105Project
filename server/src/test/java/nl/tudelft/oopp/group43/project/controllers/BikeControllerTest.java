6ec0cf1b1e4c6ad10ef13271c81b2658f4b36322
Ziang Qiu <z.qiu@student.tudelft.nl>
2020-03-22 17:57:38 +0100

    init the tests

--- server/src/test/java/nl/tudelft/oopp/group43/project/controllers/BikeControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/group43/project/controllers/BikeControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-
 @RunWith(SpringRunner.class)
-@WebMvcTest(UserController.class)
+@WebMvcTest(BikeController.class)
 @EnableAutoConfiguration
-class UserControllerTest {
+class BikeControllerTest {
+
+    private String token;
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void loginTest() throws Exception {
-
+    @BeforeEach
+    void init() throws Exception {
         MvcResult result = mockMvc.perform(post("/token")
                 .contentType("application/json")
                 .param("username","ziang.qiu@gmail.com")
@@ -33,11 +34,21 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
         String response = result.getResponse().getContentAsString();
-        String token = JsonPath.read(response,"$.token");
-        mockMvc.perform(get("/hello")
-                .header("authorization", "Bearer " + token)
+        token = JsonPath.read(response,"$.token");
+    }
+
+    @Test
+    void getBikeTest() throws Exception {
+        mockMvc.perform(get("/bike/0")
                 .contentType("application/json"))
                 .andExpect(status().isOk());
+    }
 
+    @Test
+    void getReservations() throws Exception {
+        mockMvc.perform(post("/bikeReservation/user")
+                .contentType("application/json")
+                .param("token",token))
+                .andExpect(status().isOk());
     }
 }
--- server/src/test/java/nl/tudelft/oopp/group43/project/controllers/ReservationControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/group43/project/controllers/ReservationControllerTest.java
@@ -1,10 +1,8 @@
 package nl.tudelft.oopp.group43.project.controllers;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,32 +10,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
 @EnableAutoConfiguration
-class UserControllerTest {
+class ReservationControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @Test
-    void loginTest() throws Exception {
-
-        MvcResult result = mockMvc.perform(post("/token")
-                .contentType("application/json")
-                .param("username","ziang.qiu@gmail.com")
-                .param("password","12345678"))
-                .andExpect(status().isOk())
-                .andReturn();
-        String response = result.getResponse().getContentAsString();
-        String token = JsonPath.read(response,"$.token");
-        mockMvc.perform(get("/hello")
-                .header("authorization", "Bearer " + token)
+    void getAllReservation() throws Exception {
+        mockMvc.perform(get("/reservation")
                 .contentType("application/json"))
                 .andExpect(status().isOk());
-
     }
 }
