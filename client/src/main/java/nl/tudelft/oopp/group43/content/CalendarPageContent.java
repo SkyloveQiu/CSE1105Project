e789b6c75c58a31a7e9547b932f458d1c54b5afd
Niels van den Dool <n.vandendool@student.tudelft.nl>
2020-03-26 16:44:55 +0100

    Added calendar template.
    - Added fxml page for calendar
    - Added content page for calendar
    - Added controller for calendar
    - Added calendarfx dependency
    - Working calendar template

---
+++ client/src/main/java/nl/tudelft/oopp/group43/content/CalendarPageContent.java
@@ -0,0 +1,42 @@
+package nl.tudelft.oopp.group43.content;
+
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+import java.time.LocalTime;
+
+public class CalendarPageContent {
+
+    private static Scene scene;
+
+    public static void addContent(Scene currentScene) {
+        scene = currentScene;
+
+        CalendarView calendarView = new CalendarView();
+
+        Calendar calendar = new Calendar("Calendar");
+        calendar.setShortName("C");
+        calendar.setStyle(Calendar.Style.STYLE1);
+
+        CalendarSource calendarSource = new CalendarSource("My Calendar");
+        calendarSource.getCalendars().add(calendar);
+
+        calendarView.getCalendarSources().setAll(calendarSource);
+        calendarView.setRequestedTime(LocalTime.now());
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(calendarView);
+        AnchorPane.setLeftAnchor(stackPane, 88.0);
+        AnchorPane.setTopAnchor(stackPane, 141.0);
+        AnchorPane.setBottomAnchor(stackPane, 0.0);
+        AnchorPane.setRightAnchor(stackPane, 0.0);
+
+        AnchorPane root = (AnchorPane) scene.lookup("#root");
+        root.getChildren().add(0, stackPane);
+    }
+
+}
