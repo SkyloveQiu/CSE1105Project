194094e79b4f2a4366bfff76337e00141886fdf1
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2020-03-29 16:36:57 +0300

    Just a quick crud adjustments and fixes for general purposes.

--- server/src/main/java/nl/tudelft/oopp/group43/project/repositories/BuildingFoodProductRepository.java
+++ server/src/main/java/nl/tudelft/oopp/group43/project/repositories/BuildingFoodProductRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 import nl.tudelft.oopp.group43.project.keys.BuildingFoodProductKey;
 import nl.tudelft.oopp.group43.project.models.Building;
 import nl.tudelft.oopp.group43.project.models.BuildingFoodProduct;
+import nl.tudelft.oopp.group43.project.models.BuildingFoodProductId;
 import nl.tudelft.oopp.group43.project.models.FoodProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BuildingFoodProductRepository extends JpaRepository<BuildingFoodProduct, BuildingFoodProductKey> {
+public interface BuildingFoodProductRepository extends JpaRepository<BuildingFoodProduct, BuildingFoodProductId> {
 
     boolean existsBuildingFoodProductByBuildingAndAndFoodProduct(Building building, FoodProduct foodProduct);
 
