7fcb5a5f70d30a957fd206cb3aa2c31e1291dfd6
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2020-03-30 04:47:38 +0300

    Test if the pipeline works again

--- server/src/test/java/nl/tudelft/oopp/group43/project/controllers/BuildingControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/group43/project/controllers/BuildingControllerTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.context.annotation.Configuration;
@@ -31,9 +32,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+
 @RunWith(SpringRunner.class)
-@ActiveProfiles( {"test"})
-@AutoConfigureMockMvc
+@WebMvcTest(Building.class)
+@EnableAutoConfiguration
 public class BuildingControllerTest {
 
     private static String token;
