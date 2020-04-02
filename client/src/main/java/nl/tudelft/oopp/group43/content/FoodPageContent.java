9efae17893b3b34abcd8cf8a421fc7a552f9515d
edegruyl <E.A.deGruyl@student.tudelft.nl>
2020-04-03 01:29:06 +0200

    temp commit

--- client/src/main/java/nl/tudelft/oopp/group43/content/FoodPageContent.java
+++ client/src/main/java/nl/tudelft/oopp/group43/content/FoodPageContent.java
@@ -6,7 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import nl.tudelft.oopp.group43.communication.ServerCommunication;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -21,6 +24,7 @@ public class FoodPageContent {
     public static void addContent(Scene currentScene) {
         scene = currentScene;
         addBuildings();
+        addFood();
     }
 
     private static void addRooms(String buildingName) {
@@ -67,5 +71,34 @@ public class FoodPageContent {
 
         buildingBox.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> addRooms(newValue));
 
+    }
+
+    private static void addFood() {
+        GridPane foodGrid = (GridPane) scene.lookup("#foodGrid");
+        JSONParser jsonParser = new JSONParser();
+        try {
+            JSONArray foodItemsJson = (JSONArray) jsonParser.parse(ServerCommunication.getFood());
+            System.out.println(foodItemsJson.toJSONString());
+
+            ArrayList<String> foodItems = new ArrayList<>();
+            for (Object obj : foodItemsJson) {
+                foodItems.add(((JSONObject) obj).toJSONString());
+            }
+
+            for (int i = 0; i < foodItems.size(); i++) {
+                RowConstraints rowConstraints = new RowConstraints();
+                rowConstraints.setPrefHeight(50);
+                foodGrid.getRowConstraints().add(rowConstraints);
+
+                Label label = new Label(foodItems.get(i));
+                label.setId(foodItems.get(i));
+                label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                //label.getStyleClass().add("foodLabels");
+
+                foodGrid.add(label, 0, i);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
