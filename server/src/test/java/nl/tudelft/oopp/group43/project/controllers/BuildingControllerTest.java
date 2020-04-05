f24e0ca7489b175068ba89de485e0343c2248991
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2020-04-05 16:24:01 +0300

    Add some tests regarding the exception dates.

--- server/src/test/java/nl/tudelft/oopp/group43/project/controllers/BuildingControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/group43/project/controllers/BuildingControllerTest.java
@@ -8,11 +8,15 @@ import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.List;
 import nl.tudelft.oopp.group43.project.models.Building;
+import nl.tudelft.oopp.group43.project.models.ExceptionDates;
 import nl.tudelft.oopp.group43.project.models.User;
 import nl.tudelft.oopp.group43.project.repositories.BuildingRepository;
+import nl.tudelft.oopp.group43.project.repositories.ExceptionDatesRepository;
 import nl.tudelft.oopp.group43.project.repositories.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,8 +29,12 @@ public class BuildingControllerTest {
 
     @Mock
     private BuildingRepository mockRepository;
+
     @Mock
     private UserRepository mockUserRepository;
+
+    @Mock
+    private ExceptionDatesRepository mockExceptionDatesRepository;
 
     @InjectMocks
     private BuildingController buildingControllerUnderTest;
@@ -99,4 +107,45 @@ public class BuildingControllerTest {
 
         assertEquals(result.getStatusCode(), HttpStatus.FORBIDDEN);
     }
+
+    @Test
+    public void testAddExceptionDate() {
+        final ExceptionDates exceptionDates = new ExceptionDates(0, new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), 0);
+
+        final User user = new User("admin@tudelft.nl", "firstName", "lastName", "password", "role", "token", new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>());
+        when(mockUserRepository.findUserByToken("token")).thenReturn(user);
+
+        final ExceptionDates exceptionDates1 = new ExceptionDates(0, new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), 0);
+        when(mockExceptionDatesRepository.save(any(ExceptionDates.class))).thenReturn(exceptionDates1);
+
+        final ResponseEntity result = buildingControllerUnderTest.addExceptionDate(exceptionDates, "token");
+
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testGetExceptionDate() {
+        final List<ExceptionDates> exceptionDates = Arrays.asList(new ExceptionDates(0, new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), 0));
+        when(mockExceptionDatesRepository.findAllByBuildingNumber(0)).thenReturn(exceptionDates);
+
+        final List<ExceptionDates> result = buildingControllerUnderTest.getExceptionDate(0);
+
+        assertNotNull(result);
+
+
+    }
+
+    @Test
+    public void testDeleteExceptionDate() {
+        final ExceptionDates exceptionDates = new ExceptionDates(0, new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), new GregorianCalendar(2019, Calendar.JANUARY, 1).getTime(), 0);
+
+        final User user = new User("admin@tudelft.nl", "firstName", "lastName", "password", "role", "token", new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>());
+        when(mockUserRepository.findUserByToken("token")).thenReturn(user);
+
+        final ResponseEntity result = buildingControllerUnderTest.deleteExceptionDate(exceptionDates, "token");
+
+        assertNotNull(result);
+    }
+
+
 }
