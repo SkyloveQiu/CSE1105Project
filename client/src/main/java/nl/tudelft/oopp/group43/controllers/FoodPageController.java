1b49b79a469a498234cabdff9858100eb7113c7d
edegruyl <E.A.deGruyl@student.tudelft.nl>
2020-04-05 03:50:44 +0200

    Added food order functionality

--- client/src/main/java/nl/tudelft/oopp/group43/controllers/FoodPageController.java
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/FoodPageController.java
@@ -1,9 +1,47 @@
 package nl.tudelft.oopp.group43.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.group43.communication.ServerCommunication;
+import nl.tudelft.oopp.group43.content.BikePageContent;
+import nl.tudelft.oopp.group43.content.FoodPageContent;
+import nl.tudelft.oopp.group43.sceneloader.SceneLoader;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FoodPageController {
-    public void orderFood(ActionEvent actionEvent) {
+    public void orderFood(ActionEvent event) throws IOException {
+        if (ServerCommunication.getToken().equals("invalid")) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("You are not logged in! Please login!");
+            alert.showAndWait();
 
+            SceneLoader.setScene("login");
+            SceneLoader sl = new SceneLoader();
+            sl.start((Stage) ((Node) event.getSource()).getScene().getWindow());
+            return;
+        }
+
+        ArrayList<String> food = FoodPageContent.getSelectedFood();
+        if (food.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("You haven't selected any food!");
+            alert.showAndWait();
+            return;
+        }
+        String processedFood = String.join("-", food);
+
+
+
+        String response = ServerCommunication.createFoodOrder(processedFood, "invalid");
+        if (!response.equals("OK")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Something went wrong!");
+            alert.showAndWait();
+            return;
+        }
     }
 }
