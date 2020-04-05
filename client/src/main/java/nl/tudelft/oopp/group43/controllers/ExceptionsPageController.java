7b695cff6151170f5663616309fe0717a1992a94
Niels van den Dool <n.vandendool@student.tudelft.nl>
2020-04-05 16:00:58 +0200

    Added confirm button.

--- client/src/main/java/nl/tudelft/oopp/group43/controllers/ExceptionsPageController.java
+++ client/src/main/java/nl/tudelft/oopp/group43/controllers/ExceptionsPageController.java
@@ -1,13 +1,9 @@
 package nl.tudelft.oopp.group43.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 
 public class ExceptionsPageController {
 
-
-    public void addDifferentHours(ActionEvent actionEvent) {
-
+    public void confirmExpection(ActionEvent actionEvent) {
     }
 }
