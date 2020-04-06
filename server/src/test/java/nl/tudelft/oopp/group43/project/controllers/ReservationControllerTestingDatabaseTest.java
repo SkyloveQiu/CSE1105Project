b5121fc945ba83d95ab0457f02101c8adf3ecec6
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2020-04-06 23:20:59 +0300

    Add test with mock database.

--- server/src/test/java/nl/tudelft/oopp/group43/project/controllers/ReservationControllerTestingDatabaseTest.java
+++ server/src/test/java/nl/tudelft/oopp/group43/project/controllers/ReservationControllerTestingDatabaseTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.group43.project.controllers;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,10 +17,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
@@ -30,12 +33,41 @@ class ReservationControllerTestingDatabaseTest {
     @Autowired
     MockMvc mockMvc;
 
+    private static String token;
+
 
     @Test
     void getAllReservation() throws Exception {
         mockMvc.perform(get("/reservation")
             .contentType("application/json"))
             .andExpect(status().isOk());
+    }
+
+    @BeforeEach
+    void init() throws Exception {
+        MvcResult result = mockMvc.perform(post("/token")
+            .contentType("application/json")
+            .param("username", "admin@tudelft.nl")
+            .param("password", "12345678"))
+            .andExpect(status().isOk())
+            .andReturn();
+        String response = result.getResponse().getContentAsString();
+
+        token = JsonPath.read(response, "$.token");
+
+    }
+
+
+    @Test
+    void testReservationAddAndEdit() throws Exception {
+        ReservationController b = new ReservationController();
+        ResultActions a = mockMvc.perform(post("/reservation?token=" + token)
+            .contentType(MediaType.APPLICATION_JSON).content("{\"first_name\":\"admin\",\"last_name\":\"admin\",\"username\":\"admin@tudelft.nl\",\"role\":\"admin\"},\"room_id\":46,\"starting_date\":\"2020-04-14T18:00:00.000+0000\",\"end_date\":\"2020-04-14T19:00:00.000+0000\",\"reservationId\":23}")
+            .contentType("application/json"));
+        assertNotNull(a);
+
+
+
     }
 
 }
